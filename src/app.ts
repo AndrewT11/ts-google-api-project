4d54f2229cb80062b2fdfab2a5ce627ab9e4c8a2
Andrew Tran <tranandrew@gmail.com>
2022-07-25 11:58:14 -0500

    setup querySelectors, formHandlers, and EventListeners

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
 // Code goes here!
+const form = document.querySelector('form')!;
+const addressInput = document.querySelector('#address')! as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+
+    // send to Google API
+    
+
+}
+
+form?.addEventListener("submit", searchAddressHandler);
