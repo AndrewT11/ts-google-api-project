582a3414e4a01b456b7f97fc91a2a88dcfa04095
Andrew Tran <tranandrew@gmail.com>
2022-07-25 12:49:06 -0500

    add googleAPI, save API key typescript type GeocodingResponse for typing of axios get reponse setup up error handling for axios fetch

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,30 @@
-// Code goes here!
+import axios from 'axios';
+
 const form = document.querySelector('form')!;
 const addressInput = document.querySelector('#address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = 'AIzaSyBlZrufv_sL6x8n9-UbFfmt52gVhWbHz58'
+
+type GeocodingResponse = {
+    result: {geometry: {location: {lat: number, lng: number}}}[]
+    status: "OK" | "ZERO-RESULTS"
+}
 
 function searchAddressHandler(event: Event) {
     event.preventDefault();
     const enteredAddress = addressInput.value;
 
-    // send to Google API
-    
 
+    // send to Google API
+    axios.get<GeocodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`)
+    .then(response => {
+        if(response.data.status !== "OK") {
+            throw new Error("Could not fetch location");
+        }
+        const coordinates = response.data.result[0].geometry.location;
+
+    })  
+    .catch(err => console.log(err))
 }
 
 form?.addEventListener("submit", searchAddressHandler);
